e6a9f7d632d36894c3dc82cc47c7423f799c3653
voruti <45130096+voruti@users.noreply.github.com>
2026-07-01 19:14:36 +0200

    Format

--- src/main/java/dev/shadowsoffire/despawntimers/Config.java
+++ src/main/java/dev/shadowsoffire/despawntimers/Config.java
@@ -9,9 +9,12 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 public class Config {
   private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
-  private static final ModConfigSpec.LongValue DESPAWN_DELAY = BUILDER.comment("The time, in ticks, that a mob may not despawn for after spawning.").defineInRange("despawn_delay", 600L, 0L, Long.MAX_VALUE);
-
   static final ModConfigSpec SPEC = BUILDER.build();
+
+  private static final ModConfigSpec.LongValue DESPAWN_DELAY =
+      BUILDER
+          .comment("The time, in ticks, that a mob may not despawn for after spawning.")
+          .defineInRange("despawn_delay", 600L, 0L, Long.MAX_VALUE);
 
   public static long despawnDelay;
 
--- src/main/java/dev/shadowsoffire/despawntimers/MobDespawnTimers.java
+++ src/main/java/dev/shadowsoffire/despawntimers/MobDespawnTimers.java
@@ -1,37 +1,40 @@
 package dev.shadowsoffire.despawntimers;
 
 import net.minecraft.world.entity.Entity;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.entity.living.MobDespawnEvent;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.living.MobDespawnEvent;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(MobDespawnTimers.MODID)
 public class MobDespawnTimers {
-    // Define mod id in a common place for everything to reference
-    public static final String MODID = "despawntimers";
+  // Define mod id in a common place for everything to reference
+  public static final String MODID = "despawntimers";
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public MobDespawnTimers(ModContainer modContainer) {
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (MobDespawnTimers) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
-        NeoForge.EVENT_BUS.register(this);
+  // The constructor for the mod class is the first code that is run when your mod is loaded.
+  // FML will recognize some parameter types like IEventBus or ModContainer and pass them in
+  // automatically.
+  public MobDespawnTimers(ModContainer modContainer) {
+    // Register ourselves for server and other game events we are interested in.
+    // Note that this is necessary if and only if we want *this* class (MobDespawnTimers) to respond
+    // directly to events.
+    // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like
+    // onServerStarting() below.
+    NeoForge.EVENT_BUS.register(this);
 
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.SERVER, Config.SPEC);
+    // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+    modContainer.registerConfig(ModConfig.Type.SERVER, Config.SPEC);
+  }
+
+  // You can use SubscribeEvent and let the Event Bus discover methods to call
+  @SubscribeEvent
+  public void onDespawn(MobDespawnEvent event) {
+    Entity ent = event.getEntity();
+    if (Config.despawnDelay >= ent.tickCount) {
+      event.setResult(MobDespawnEvent.Result.DENY);
     }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onDespawn(MobDespawnEvent event) {
-        Entity ent = event.getEntity();
-        if (Config.despawnDelay >= ent.tickCount) {
-            event.setResult(MobDespawnEvent.Result.DENY);
-        }
-    }
+  }
 }
